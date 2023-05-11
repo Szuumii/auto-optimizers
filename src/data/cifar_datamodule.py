312a281e9db4a980186e30905d746f4d2e0d9c32
Szuumii <jakub.szumskii@gmail.com>
2023-05-11 20:40:37 +0200

    feat: delete files and apply minor corrections

--- src/data/cifar_datamodule.py
+++ src/data/cifar_datamodule.py
@@ -24,7 +24,10 @@ class CIFARDataModule(LightningDataModule):
 
         # data transformations
         self.transforms = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
         )
 
         self.data_train: Optional[Dataset] = None
